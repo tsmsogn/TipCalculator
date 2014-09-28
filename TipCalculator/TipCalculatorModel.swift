7c031d1a64c2f057a675ccf26543408417eb2b88
tsmsogn <tsmsogn@gmail.com>
2014-09-28 15:00:04 +0900

    Add TipCalculator

---
+++ TipCalculator/TipCalculatorModel.swift
@@ -0,0 +1,41 @@
+//
+//  TipCalculatorModel.swift
+//  TipCalculator
+//
+//  Created by tsmsogn on 2014/09/26.
+//  Copyright (c) 2014年 tsmsogn. All rights reserved.
+//
+
+import Foundation
+
+class TipCalculatorModel {
+    var total: Double
+    var taxPct: Double
+    var subtotal: Double {
+        get {
+            return total / (taxPct + 1)
+        }
+    }
+    
+    init(total:Double, taxPct:Double) {
+        self.total = total
+        self.taxPct = taxPct
+    }
+    
+    func calcTipWithTipPct(tipPct:Double) -> Double {
+        return subtotal * tipPct
+    }
+    
+    func returnPossibleTips() -> [Int: Double] {
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplict:[Double] = [0.15, 0.18, 0.20] // [Double] is shotcut Array<Double>
+        
+        var retval = [Int: Double]()
+        for possibleTip in possibleTipsInferred {
+            let intPct = Int(possibleTip*100)
+            retval[intPct] = calcTipWithTipPct(possibleTip)
+        }
+        return retval
+    }
+    
+}
